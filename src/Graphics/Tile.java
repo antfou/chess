3d8280a0da11970834035b3431d6063990984c03
antfou <fougneranton@gmail.com>
2022-11-05 21:13:10 +0100

    initial commit

---
+++ src/Graphics/Tile.java
@@ -0,0 +1,55 @@
+package Graphics;
+
+public abstract class Tile {
+
+    int tileNR;
+    boolean color;
+
+    Tile(int tileNR){
+        this.tileNR = tileNR;
+        this.color = color;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int nr) {
+            super(nr);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+
+    }
+
+    public static final class OccupiedTile extends Tile{
+
+        Piece pieceOnTile;
+
+        OccupiedTile(int nr, Piece pieceOnTile){
+            super(nr);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+
+
+}
---
+++ src/Logic/BoardLogic.java
@@ -0,0 +1,29 @@
+package Logic;
+
+import java.util.Vector;
+
+public class BoardLogic {
+
+
+
+
+
+
+
+
+
+
+
+    /*void BoardBuilder(){
+        for (int x = 0; x < 8; x++){
+            for (int y = 0; y < 8; y++) {
+                boolean isWhiteSquare = (y + x) % 2 != 0;
+
+                var squareColor = (isWhiteSquare) ? whiteCol : blackCol;
+                //var position = new Vector<> (-3.5f + x, 3.5f + y);
+            }
+        }
+    }
+
+     */
+}
