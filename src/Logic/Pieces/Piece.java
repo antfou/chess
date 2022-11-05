006842849a65cf55585cedd1c852a6da244e6e2a
antfou <fougneranton@gmail.com>
2022-11-05 21:48:58 +0100

    Tile subclasses improved immutability, added Collections.unmodifiableMap in Tile class .

--- src/Logic/Pieces/Piece.java
+++ src/Logic/Pieces/Piece.java
@@ -1,4 +1,14 @@
 package Logic.Pieces;
 
+import Logic.Players.Side;
+
 public class Piece {
+
+    protected final int pieceNR;
+    protected final Side pieceSide;
+
+    Piece(final int pieceNR, final Side pieceSide){
+        this.pieceNR = pieceNR;
+        this.pieceSide = pieceSide;
+    }
 }
---
+++ src/Logic/Players/Side.java
@@ -0,0 +1,4 @@
+package Logic.Players;
+
+public enum Side {
+}
